e8499513d18ac341edfede98f7719bcb556714eb
Stephen Eneji <enejistephen01@gmail.com>
2022-06-24 00:09:15 +0100

    Add files via upload

--- My Portfolio/blog.js
+++ My Portfolio/blog.js
@@ -21,3 +21,21 @@ let searchButton = document.getElementById('search-blog')
 searchButton.addEventListener('click', function(e){
     e.target.style.opacity = "1";
 })
+
+// /* Reaction Up JS */
+// let reactionUp = document.getElementById('r1-blog')
+
+// reactionUp.addEventListener('click', function(e) {
+//     e.target.style.backgroundColor = "green";
+//     e.target.style.color = "white";
+// })
+
+
+// /* Reaction Down JS */
+// let reactionDown = document.getElementById('r2-blog')
+
+// reactionDown.addEventListener('click', function(e) {
+//     e.target.style.backgroundColor = "red";
+//     e.target.style.color = "white";
+
+// })
